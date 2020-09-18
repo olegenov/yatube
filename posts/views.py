7f8ee1f70f47193110791407bd6942b7c33a0b3b
Никита Китаев <nik_kitaev@mail.ru>
2020-09-18 18:06:17 +0300

    Commit

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
 from django.urls import reverse
@@ -66,11 +67,12 @@ def new_post(request):
 
 def profile(request, username):
     profile = get_object_or_404(User, username=username)
-    with open('media/profile.jpg','rb') as img:
-        try:
-            photo = ProfilePhoto.objects.get(user=profile).photo
-        except:
-            photo = img
+    
+    try:
+        photo = ProfilePhoto.objects.get(user=profile).photo
+    except:
+        photo = None
+    
     post_list = profile.posts.order_by('-pub_date')
     post_amount = post_list.count()
     paginator = Paginator(post_list, 10)
@@ -105,11 +107,12 @@ def profile(request, username):
 
 def post_view(request, username, post_id):
     profile = get_object_or_404(User, username=username)
-    with open('media/profile.jpg','rb') as img:
-        try:
-            photo = ProfilePhoto.objects.get(user=profile).photo
-        except:
-            photo = img
+
+    try:
+        photo = ProfilePhoto.objects.get(user=profile).photo
+    except:
+        photo = None
+
     post = get_object_or_404(Post, pk=post_id, author=profile)
     post_amount = profile.posts.count()
     form = CommentForm()
