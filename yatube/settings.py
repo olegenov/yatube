9cca4690604c68e909d8c6bd06e2b6cc08505896
olegenov <nik_kitaev@mail.ru>
2021-01-30 23:52:17 +0300

    Server commit

--- yatube/settings.py
+++ yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*5kh1g%wbhi7)0#1vvlc@wbyjl#-m@zbvci!v+npjsk32(k&g@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
